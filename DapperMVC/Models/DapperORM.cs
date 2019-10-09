758e17c55363fa0ca911928a37eef09b5880c95e
Victor da Silva <Victor10019@hotmail.com>
2019-10-09 14:03:07 -0300

    exemplo crud com dapper

---
+++ DapperMVC/Models/DapperORM.cs
@@ -0,0 +1,43 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DapperMVC.Models
+{
+    public static class DapperORM
+    {
+        private static string connectionString = @"Data Source=DESKTOP-GCQGKNO;Initial Catalog=DapperDB;Integrated Security=True;";
+        public static void ExecuteWithoutReturn(string procedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection sqlcon = new SqlConnection(connectionString))
+            {
+                sqlcon.Open();
+                sqlcon.Execute(procedureName, param, commandType: CommandType.StoredProcedure);
+            }
+        }
+
+        public static T ExecuteReturnScalar<T>(string procedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection sqlcon = new SqlConnection(connectionString))
+            {
+                sqlcon.Open();
+               return (T)Convert.ChangeType( sqlcon.Execute(procedureName, param, commandType: 
+                   CommandType.StoredProcedure), typeof(T));
+            }
+        }
+
+        public static IEnumerable<T> ReturnList<T>(string procedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection sqlcon = new SqlConnection(connectionString))
+            {
+                sqlcon.Open();
+                return sqlcon.Query<T>(procedureName, param, commandType: CommandType.StoredProcedure);
+            }
+        }
+
+    }
+}
---
+++ DapperMVC/Models/EmployeeModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DapperMVC.Models
+{
+    public class EmployeeModel
+    {
+        public int EmployeeID { get; set; }
+
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public string Age { get; set; }
+        public double Salary { get; set; }
+
+    }
+}
